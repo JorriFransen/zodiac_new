62d64c09781b56b3e82ca82fc5a993ccb1ecc958
jorri <jorri>
2020-08-03 22:20:29 +0000

    Builtin cleanup.

--- include/zodiac/builtin.h
+++ include/zodiac/builtin.h
@@ -3,23 +3,47 @@
 #include "struct_predecls.h"
 #include "atom.h"
 
+enum Builtin_Type_Kind
+{
+    INVALID,
+
+    VOID,
+    INTEGER,
+};
+
+#define BUILTIN_TYPE_LIST                        \
+    DEFINE_BUILTIN_TYPE(void, VOID, 0, false)    \
+    DEFINE_BUILTIN_TYPE(s64, INTEGER, 64, true)  \
+    DEFINE_BUILTIN_TYPE(u64, INTEGER, 64, false) \
+
+#define BUILTIN_ATOM_LIST      \
+    DEFINE_BUILTIN_ATOM(exit)  \
+    DEFINE_BUILTIN_ATOM(naked) \
+
+#undef DEFINE_BUILTIN_TYPE
+
 namespace Zodiac
 {
     struct Builtin
     {
         static uint64_t pointer_size;
 
-        static Atom atom_exit;
-        static Atom atom_naked;
-        static Atom atom_s64;
+#define DEFINE_BUILTIN_ATOM(name) static Atom atom_ ##name;
+#define DEFINE_BUILTIN_TYPE(name, kind, size, signed) static Atom atom_ ##name;
+        BUILTIN_TYPE_LIST
+        BUILTIN_ATOM_LIST
+#undef DEFINE_BUILTIN_ATOM
+#undef DEFINE_BUILTIN_TYPE
 
-        static AST_Type *type_void;
-        static AST_Type *type_s64;
-        static AST_Type *type_u64;
+#define DEFINE_BUILTIN_TYPE(name, kind, size, signed) static AST_Type *type_ ##name;
+        BUILTIN_TYPE_LIST
+#undef DEFINE_BUILTIN_TYPE
     };
 
     void builtin_initialize_atoms(Atom_Table* at);
     void builtin_initialize_types(Allocator *allocator);
+    AST_Type *builtin_initialize_type(Allocator *allocator, Builtin_Type_Kind kind, uint64_t size,
+                                      bool sign);
 
     void builtin_populate_scope(Allocator *allocator, Scope *global_scope);
 }
--- source/builtin.cpp
+++ source/builtin.cpp
@@ -8,27 +8,52 @@ namespace Zodiac
 
 uint64_t Builtin::pointer_size = 64;
 
-Atom Builtin::atom_exit = {};
-Atom Builtin::atom_naked = {};
-Atom Builtin::atom_s64 = {};
+#define DEFINE_BUILTIN_ATOM(name) Atom Builtin::atom_ ##name = {};
+#define DEFINE_BUILTIN_TYPE(name, kind, size, signed) Atom Builtin::atom_ ##name = {};
+BUILTIN_TYPE_LIST
+BUILTIN_ATOM_LIST
+#undef DEFINE_BUILTIN_ATOM
+#undef DEFINE_BUILTIN_TYPE
 
-AST_Type *Builtin::type_void = nullptr;
-AST_Type *Builtin::type_s64 = nullptr;
-AST_Type *Builtin::type_u64 = nullptr;
+#define DEFINE_BUILTIN_TYPE(name, kind, size, signed) AST_Type *Builtin::type_ ##name = {};
+BUILTIN_TYPE_LIST
+#undef DEFINE_BUILTIN_TYPE
+
+#include <stdio.h>
 
 void builtin_initialize_atoms(Atom_Table* at)
 {
-    Builtin::atom_exit = atom_get(at, "exit");
-    Builtin::atom_naked = atom_get(at, "naked");
-    Builtin::atom_s64 = atom_get(at, "s64");
+#define DEFINE_BUILTIN_ATOM(name) Builtin::atom_ ##name = atom_get(at, #name);
+#define DEFINE_BUILTIN_TYPE(name, kind, size, sign) Builtin::atom_ ##name = atom_get(at, #name);
+    BUILTIN_TYPE_LIST
+    BUILTIN_ATOM_LIST
+#undef DEFINE_BUILTIN_ATOM
+#undef DEFINE_BUILTIN_TYPE
 }
 
 void builtin_initialize_types(Allocator *allocator)
 {
-    Builtin::type_void = ast_type_new(allocator, AST_Type_Kind::VOID, 0);;
+#define DEFINE_BUILTIN_TYPE(name, kind, size, sign) \
+    Builtin::type_ ##name = builtin_initialize_type(allocator, Builtin_Type_Kind::kind, size, sign);
 
-    Builtin::type_s64 = ast_integer_type_new(allocator, 64, true);
-    Builtin::type_u64 = ast_integer_type_new(allocator, 64, false);
+    BUILTIN_TYPE_LIST
+
+#undef DEFINE_BUILTIN_TYPE
+}
+
+AST_Type *builtin_initialize_type(Allocator *allocator, Builtin_Type_Kind kind, uint64_t size,
+                                  bool sign)
+{
+    switch (kind)
+    {
+        case Builtin_Type_Kind::INVALID: assert(false);
+
+        case Builtin_Type_Kind::VOID: 
+            return ast_type_new(allocator, AST_Type_Kind::VOID, size);
+
+        case Builtin_Type_Kind::INTEGER: 
+            return ast_integer_type_new(allocator, size, sign);
+    }
 }
 
 void builtin_populate_scope(Allocator *allocator, Scope *global_scope)
@@ -37,9 +62,16 @@ void builtin_populate_scope(Allocator *allocator, Scope *global_scope)
 
     File_Pos fp = { 0, 0, 0, string_ref("builtin") };
 
-    auto s64_ident = ast_identifier_new(allocator, Builtin::atom_s64, fp, fp);
-    auto s64_decl = ast_type_declaration_new(allocator, Builtin::type_s64, s64_ident);
-    scope_add_declaration(allocator, global_scope, s64_decl);
+    #define DEFINE_BUILTIN_TYPE(name, kind, size, signed) { \
+        auto ident = ast_identifier_new(allocator, Builtin::atom_##name, fp, fp); \
+        auto decl = ast_type_declaration_new(allocator, Builtin::type_##name, ident); \
+        scope_add_declaration(allocator, global_scope, decl); \
+    } 
+
+    BUILTIN_TYPE_LIST
+
+    #undef DEFINE_BUILTIN_TYPE
 }
+
 
 }
