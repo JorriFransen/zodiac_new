199cc615b7d90439fcc808b3a5a4798dc0721dbd
Jorri Fransen <jorri@jorrifransen.nl>
2021-02-09 23:42:43 +0100

    Also running generated exe's in the run_all_tests script.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -8,9 +8,23 @@ for f in $wd/tests/*.zdc
 do
     echo "==================================================================="
     echo "===================================================================\n"
+    echo COMPILING AND RUNNING BYTECODE
     echo  Running command: ./zodiac $f
     echo
     ./zodiac $f
+    echo
+    _exe_name=${f%.*}
+    exe_name=./${_exe_name#"$wd"/tests/}
+    if [ -e $exe_name ]
+    then
+        echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        echo RUNNING EXE: $exe_name
+        echo
+        eval $exe_name
+        rm $exe_name
+        rm $exe_name.o
+    fi
+    echo
     echo "==================================================================="
     echo "===================================================================\n\n"
 done
