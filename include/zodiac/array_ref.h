64bb8e240b418de2415da45eb58c53354e3f9dde
Jorri Fransen <jorri@jorrifransen.nl>
2021-05-05 15:11:16 +0200

    Added Array_Ref class.

---
+++ include/zodiac/array_ref.h
@@ -0,0 +1,39 @@
+
+#pragma once
+
+#include "allocator.h"
+#include "array.h"
+
+#include "common.h"
+
+namespace Zodiac
+{
+
+template <typename Element_Type>
+struct Array_Ref
+{
+    const Element_Type *data = nullptr;
+    int64_t count = 0;
+
+    Array_Ref() : data(nullptr), count(0) {}
+
+    template <size_t N>
+    constexpr Array_Ref(const Element_Type (&c_arr)[N]) : data(c_arr), count(N) {}
+
+    constexpr Array_Ref(const Array<Element_Type> &arr) : data(arr.data), count(arr.count) {}
+
+    Element_Type& operator[](int index)
+    {
+        assert(index < count);
+        return data[index];
+    }
+
+    const Element_Type& operator[](int index) const
+    {
+        assert(index < count);
+        return data[index];
+    }
+};
+
+
+}
